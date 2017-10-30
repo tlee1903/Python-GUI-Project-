443134792ffdf3e8b50c3543eb7b5612d5754a86
tlee1903 <tlee1903@mpsedu.org>
2017-10-30 09:56:17 -0500

    Update Python GUI Project.py

--- Python GUI Project.py
+++ Python GUI Project.py
@@ -1,13 +1,14 @@
 from Tkinter import *
 root = Tk()
-master = Tk()
 
-editor = Text(width=30, height=15)
-editor.grid(row=2, column=1, rowspan=2, sticky=SE)
+root.title("Tic-Tac-Toe")
 
+canvas= Canvas(root, background='white', width=600, height=600)
+canvas.grid(row=1, column=1)
+canvas.create_line(600, 200, 0, 200, fill='black')
+canvas.create_line(600, 400, 0, 400, fill='black')
+canvas.create_line(200, 0, 200, 600, fill='black')
+canvas.create_line(400, 0, 400, 600, fill='black')
 
-
-root.title("Adventure Game")
-root.geometry("520x240+0+0")
 
 root.mainloop()
