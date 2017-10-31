b870bd2363ae575f7d109d655c680be932e70c4e
tlee1903 <tlee1903@mpsedu.org>
2017-10-31 10:22:45 -0500

    Update Python GUI Project.py

--- Python GUI Project.py
+++ Python GUI Project.py
@@ -1,4 +1,7 @@
 from Tkinter import *
+import PIL 
+import os.path 
+from PIL import ImageTk
 root = Tk()
 
 root.title("Tic-Tac-Toe")
@@ -10,5 +13,26 @@ canvas.create_line(600, 400, 0, 400, fill='black')
 canvas.create_line(200, 0, 200, 600, fill='black')
 canvas.create_line(400, 0, 400, 600, fill='black')
 
+__dir__ = os.path.dirname(os.path.abspath(__file__))  
+filename = os.path.join(__dir__, 'x.png')
+
+img = PIL.Image.open(filename)
+img=img.convert('RGBA')
+tkimg = PIL.ImageTk.PhotoImage(img)
+def stamp(event):
+    canvas.create_image(event.x, event.y, image=tkimg)
+    
+canvas.bind('<ButtonPress-1>', stamp)
+
+__dir__ = os.path.dirname(os.path.abspath(__file__))
+filename1 = os.path.join(__dir__, 'o.png')
+
+img1 = PIL.Image.open(filename1)
+img1=img1.convert('RGBA')
+tkimg1 = PIL.ImageTk.PhotoImage(img1)
+def stamp1(event):
+    canvas.create_image(event.x, event.y, image=tkimg1)
+    
+canvas.bind('<ButtonPress-3>', stamp1)
 
 root.mainloop()
